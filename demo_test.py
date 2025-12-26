6cac30abae270eac4850915483f7d1cf281b7a90
AbhijitGaikwad <abhijit.gaikwad@neutrinotechlabs.com>
2025-12-26 16:51:01 +0530

    adding demo_test 7

--- demo_test.py
+++ demo_test.py
@@ -94,13 +94,8 @@ def health_check():
 # -----------------------------
 # Health Check
 # -----------------------------
+@app.get("/health")
+def health_check():
+    return {"status": "Calculator API is running"}
 
-
-# -----------------------------
-# Health Check
-# -----------------------------
-
-# -----------------------------
-# Health Check
-# -----------------------------
 
